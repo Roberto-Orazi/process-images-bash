47fe4143fb3909fa8d5f7faaeffa929a602a1acf
roberto <robervgg@gmail.com>
2023-06-29 19:30:44 -0300

    Add: finish the generate, it has to be reviewed

--- generador/generador.sh
+++ generador/generador.sh
@@ -1,2 +1,22 @@
 #!/bin/env bash
 
+if [ $# -ne 1 ]; then
+  echo "Uso: generar.sh <cantidad_imagenes>"
+  exit 1
+fi
+
+cantidad_imagenes=$1
+archivo_nombres="nombres.txt"
+
+
+for ((i=1; i<=$cantidad_imagenes; i++)); do
+  nombre_archivo=$(shuf -n 1 $archivo_nombres)
+  wget -O $nombre_archivo.jpg "https://thispersondoesnotexist.com/image"
+  sleep 1
+done
+
+tar -czvf imagenes.tar.gz *.jpg
+
+md5sum imagenes.tar.gz > verificacion.txt
+
+echo "Generación de imágenes completada."
