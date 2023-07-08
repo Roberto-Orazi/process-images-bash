7765ef42a6d3cd0058a1775998080f2a30c8512d
Roberto-Orazi <robervgg@gmail.com>
2023-07-08 17:45:35 -0300

    Prueba de descomprimir

--- bash/script/descomprimir.sh
+++ bash/script/descomprimir.sh
@@ -1,1 +1,17 @@
 #/!bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Error: Debes proporcionar dos archivos como argumentos."
+  exit 1
+fi
+
+compressed_file=$1
+checksum_file=$2
+
+
+if ! md5sum --status -c "$checksum_file"; then
+  echo "Error: La suma de verificación no coincide. No se puede descomprimir."
+  exit 1
+fi
+
+unzip "$compressed_file"
