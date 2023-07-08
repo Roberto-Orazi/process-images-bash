1867ceb93529140240f861c72fa15f84cdf36f85
Roberto-Orazi <robervgg@gmail.com>
2023-07-08 17:48:24 -0300

    Updated branch

--- bash/script/procesar.sh
+++ bash/script/procesar.sh
@@ -1,1 +1,7 @@
 #!/bin/env bash
+
+for file in *.jpg; do
+  if [[ $file =~ ^[A-Z][a-z]+\.jpg$ ]]; then
+    convert "$file" -gravity center -resize 512x512+0+0 -extent 512x512 "processed_$file"
+  fi
+done
