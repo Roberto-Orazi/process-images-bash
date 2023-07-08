e971b7d0ca5950f178ee46f51650ea3a3d927918
Roberto-Orazi <robervgg@gmail.com>
2023-07-08 17:50:16 -0300

    Update branch and files

--- bash/script/comprimir.sh
+++ bash/script/comprimir.sh
@@ -1,1 +1,9 @@
 #!/bin/env bash
+
+ls *.jpg > image_list.txt
+
+grep "^[A-Z][a-z]\+\.jpg$" image_list.txt > valid_names.txt
+
+grep "a\.jpg$" valid_names.txt | wc -l > total_a.txt
+
+zip compressed_files.zip image_list.txt valid_names.txt total_a.txt *.jpg
