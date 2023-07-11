82d4876ef65271cfa96aa2dc598fb3ccc41714dc
Roberto-Orazi <robervgg@gmail.com>
2023-07-11 19:39:20 -0300

    Comente la funcion descomprimir

--- bash/script/descomprimir.sh
+++ bash/script/descomprimir.sh
@@ -1,14 +1,14 @@
 #/!bin/bash
-
+#Aca hacemos un if para verificar que se ingresen dos archivos como argumentos
 if [ $# -ne 2 ]; then
   echo "Error: Debes proporcionar dos archivos como argumentos."
   exit 1
 fi
-
+#Aca asignamos los argumentos a las variables
 compressed_file=$1
 checksum_file=$2
 
-
+#Aca verificamos que el archivo comprimido exista
 if ! md5sum --status -c "$checksum_file"; then
   echo "Error: La suma de verificación no coincide. No se puede descomprimir."
   exit 1
