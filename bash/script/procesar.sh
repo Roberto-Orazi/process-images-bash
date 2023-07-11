a90684031975f9d3d6fb435feb79adc85302bbfe
Roberto-Orazi <robervgg@gmail.com>
2023-07-11 19:48:06 -0300

    Comente la funcion procesar

--- bash/script/procesar.sh
+++ bash/script/procesar.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-
+#Este script procesa las images que cumplen con el regex y las convierte a otra resolución
 for file in *.jpg; do
   if [[ $file =~ ^[A-Z][a-z]+\.jpg$ ]]; then
     convert "$file" -gravity center -resize 512x512+0+0 -extent 512x512 "processed_$file"
